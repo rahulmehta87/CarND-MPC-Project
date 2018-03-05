699ea8550a1dae1663979f6749085b47293c5854
Rahul Mehta <rahulmehta87@gmail.com>
2018-03-04 22:41:45 -0800

    Code and Documentation for the MPC Project

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ using json = nlohmann::json;
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+const double Lf = 2.67;
+double latency = 0.1;
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -79,6 +81,10 @@ int main() {
     string sdata = string(data).substr(0, length);
     cout << sdata << endl;
     if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
+
+      // Record start time
+      auto start = std::chrono::high_resolution_clock::now();
+
       string s = hasData(sdata);
       if (s != "") {
         auto j = json::parse(s);
@@ -91,21 +97,50 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double delta = j[1]["steering_angle"];
+          double a = j[1]["throttle"];
 
-          /*
-          * TODO: Calculate steering angle and throttle using MPC.
-          *
-          * Both are in between [-1, 1].
-          *
-          */
-          double steer_value;
-          double throttle_value;
+          v = v * 0.44704; // conversion from miles per hour to meters per second
+          a = a * 5; // conversion to meters per second squared
+          delta = -delta * deg2rad(25); // conversion to radians
+
+          // Compensation for latency
+          px = px + v * cos(psi) * latency;
+          py = py + v * sin(psi) * latency;
+          psi = psi + (v * tan(delta) / Lf) * latency + ((a * tan(delta) / (2 * Lf)) * pow(latency, 2));
+          v = v + a * latency;
+
+          // Conversion from map coordinates to vehicle coordinates
+          Eigen::VectorXd pts_x(ptsx.size());
+          Eigen::VectorXd pts_y(ptsy.size());
+          for (int i = 0; i < ptsx.size(); i++) {
+            double x_diff = ptsx[i] - px;
+            double y_diff = ptsy[i] - py;
+            double r = sqrt(x_diff * x_diff + y_diff * y_diff);
+            double theta = atan2(y_diff, x_diff) - psi;
+            pts_x[i] = r * cos(theta);
+            pts_y[i] = r * sin(theta);
+          }
+
+          // Evaluation of cte and epsi using polynomial fitting
+          auto coeffs = polyfit(pts_x, pts_y, 3);
+          double cte = -polyeval(coeffs, 0);
+          double epsi = -atan(coeffs[1]);
+
+          // Initialization of state
+          Eigen::VectorXd state(6);
+          state << 0, 0, 0, v, cte, epsi;
+
+          // Solve
+          vector<double> input = mpc.Solve(state, coeffs);
+
+          // Obtaining actuator values from the solution
+          double steer_value = input[0];
+          double throttle_value = input[1];
 
           json msgJson;
-          // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
-          // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = throttle_value;
+          msgJson["steering_angle"] = -steer_value / deg2rad(25); // normalization into [-1, 1] range
+          msgJson["throttle"] = throttle_value / 5; // normalization into [-1, 1] range
 
           //Display the MPC predicted trajectory 
           vector<double> mpc_x_vals;
@@ -113,6 +148,8 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
+          mpc_x_vals = mpc.mpc_x_vals;
+          mpc_y_vals = mpc.mpc_y_vals;
 
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
@@ -123,6 +160,11 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
+
+          for (int i = 0; i < pts_x.size(); i++) {
+            next_x_vals.push_back(pts_x[i]);
+            next_y_vals.push_back(pts_y[i]);
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
@@ -147,6 +189,11 @@ int main() {
         std::string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
+
+      // Record end time and update latency by averaging previous and current values
+      auto finish = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> elapsed = finish - start;
+      latency = (latency + elapsed.count()) / 2.0;
     }
   });
 
